a2767509c7b260778c9b083ba98a65caf9e44617
shivanix <shvni.here@gmail.com>
2021-07-19 22:02:19 +0000

    created file; queried all cohorts with 18 or more students.

--- 3_queries_group-by/q_rowswith_total_asmts_greaterEqual_10.sql
+++ 3_queries_group-by/q_rowswith_total_asmts_greaterEqual_10.sql
@@ -1,4 +1,4 @@
---rows with the total number of assignments greater than or equal to 10
+-- BUSY DAYS - rows with the total number of assignments greater than or equal to 10
 
 SELECT day, count(*) as total_assignments 
 FROM assignments
