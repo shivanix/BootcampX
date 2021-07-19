703e598dc636a9d0225824a78979b1298b455941
shivanix <shvni.here@gmail.com>
2021-07-19 21:58:30 +0000

    created file; rows where total assignments is greater than or equal to 10

---
+++ 3_queries_group-by/q_rowswith_total_asmts_greaterEqual_10.sql
@@ -0,0 +1,7 @@
+--rows with the total number of assignments greater than or equal to 10
+
+SELECT day, count(*) as total_assignments 
+FROM assignments
+GROUP BY day
+HAVING count(*) >= 10
+ORDER BY day;
