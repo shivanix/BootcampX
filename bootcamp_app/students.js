26ff93960764f1518c01b5315ea0a35f8606abac
shivanix <shvni.here@gmail.com>
2021-07-22 00:41:39 +0000

    added queries to select some data about students

---
+++ bootcamp_app/students.js
@@ -0,0 +1,57 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+/*------------------------------- q to get the cohort_id from students-----------------------------*/
+/*
+pool.query(`
+SELECT id, name, cohort_id
+FROM students
+LIMIT 5;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_id} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
+
+*/
+
+/*-------------------------query to get the cohort name instead of cohort_id--------------------------*/
+/*
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+LIMIT 5;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
+*/
+
+/*---------------------------- --q to get students data, while giving input thru command line------------*/
+
+//insert process.argv directly to the query
+//q to allow a user to specify a cohort name and the limit.
+ //So someone should be able to run the application with the following command:
+
+ pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
