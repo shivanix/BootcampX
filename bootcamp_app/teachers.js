9b536a7b3d81799c34829e68fb1eb71a07a06dbe
shivanix <shvni.here@gmail.com>
2021-07-22 01:33:20 +0000

    parameterized q on teachers.js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -10,16 +10,18 @@ const pool = new Pool({
 
 
 //Accepted the cohort name as input from the user.
+let teachersArgue =[`${process.argv[2]}`];
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+let query = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+WHERE cohorts.name = $1
+ORDER BY teacher;`
+
+
+pool.query(query, teachersArgue)
 .then(res => {
   res.rows.forEach(row => {
     //console.log(row)
