bceb22374fa72b4299425008c2c4d19b1a235e1f
shivanix <shvni.here@gmail.com>
2021-07-23 01:32:00 +0000

    Updated students.js and teachers.js to use parameterized queries.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -41,7 +41,7 @@ LIMIT 5;
 
 //insert process.argv directly to the query
 //q to allow a user to specify a cohort name and the limit.
- //So someone should be able to run the application with the following command:
+//So someone should be able to run the application with the following command:
 
 //  pool.query(`
 // SELECT students.id as student_id, students.name as name, cohorts.name as cohort
@@ -58,7 +58,7 @@ LIMIT 5;
 
 
 
-/*------------------------------------------*/
+/*----------------------------------Parameterized queries----------------------------*/
 
 let text = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
@@ -69,9 +69,9 @@ LIMIT $2;`;
 const cohortName = process.argv[2];
 const limit = process.argv[3] || 5;
 const values = [`%${cohortName}%`, limit];
- pool.query(text, values)
- .then(res => {
-   res.rows.forEach(user => {
-     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-   })
- }).catch(err => console.error('query error', err.stack));
+pool.query(text, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    });
+  }).catch(err => console.error('query error', err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,9 +8,9 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
+/*--------------------------Parameterized queries---------------------*/
 //Accepted the cohort name as input from the user.
-let teachersArgue =[`${process.argv[2]}`];
+let teachersArgue = [`${process.argv[2]}`];
 
 let query = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
@@ -18,13 +18,13 @@ JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = $1
-ORDER BY teacher;`
+ORDER BY teacher;`;
 
 
 pool.query(query, teachersArgue)
-.then(res => {
-  res.rows.forEach(row => {
+  .then(res => {
+    res.rows.forEach(row => {
     //console.log(row)
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-}).catch(err => console.error('query error', err.stack));
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  }).catch(err => console.error('query error', err.stack));
